0a0b8e32134d50b1aadc9d83351e35e35a8913ad
DeNeRATe-cool <61106170+DeNeRATe-cool@users.noreply.github.com>
2025-08-29 00:27:12 +0800

    Update axios.js
    Signed-off-by: DeNeRATe-cool <61106170+DeNeRATe-cool@users.noreply.github.com>

--- src/js/axios.js
+++ src/js/axios.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 //     }
 // });
 const instance = axios.create({
-    baseURL: 'http://se-backend.se:8080',
+    baseURL: 'http://localhost:30080',
     timeout: 50000
 });
 
