c416c91454b00f62572ad3767a5debda72b88b03
DeNeRATe-cool <1229836346@qq.com>
2025-08-30 11:22:37 +0800

    fix CORS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9080',
+        changeOrigin: true,
+        rewrite: p => p.replace(/^\/api/, '')
+      }
+    }
+  }
 })
