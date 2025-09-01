404090b41ceded50f5e469bdaa63fde26cb6a9ce
DeNeRATe-cool <1229836346@qq.com>
2025-09-01 21:14:31 +0800

    final test

--- src/js/axios.js
+++ src/js/axios.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 //     }
 // });
 const instance = axios.create({
-    baseURL: '/api',
+    baseURL: 'http://localhost:30080',
     timeout: 50000
 });
 
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:30080',
+//         changeOrigin: true,
+//         rewrite: p => p.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// })
+
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:30080',
-        changeOrigin: true,
-        rewrite: p => p.replace(/^\/api/, '')
-      }
-    }
-  }
 })
